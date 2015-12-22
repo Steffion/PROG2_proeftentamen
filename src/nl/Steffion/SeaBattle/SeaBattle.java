2112f4f9d89a9fbcf3fb741de38c0ca445a20070
Steffion <steffion@icloud.com>
2015-12-22 22:35:10 +0100

    Added temporary methods

--- src/nl/Steffion/SeaBattle/SeaBattle.java
+++ src/nl/Steffion/SeaBattle/SeaBattle.java
@@ -1,10 +1,19 @@
 package nl.Steffion.SeaBattle;
 
 public class SeaBattle {
-	private Player player1;
-	private Player player2;
-	
-	public void play() {
+	public static final boolean	CHEAT	= true;
+	private Player				player1;
+	private Player				player2;
+
+	public SeaBattle() {
+		player1 = new Player();
+		player2 = new Player();
+		player1.getField().placeShipsRandomly();
+		player2.getField().placeShipsRandomly();
 		
+	}
+
+	public void play() {
+		player1.getField().print();
 	}
 }
