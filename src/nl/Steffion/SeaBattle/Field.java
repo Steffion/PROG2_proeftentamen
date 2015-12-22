2da56131138af8912426cbc7819d3dfa82dfe4d0
Steffion <steffion@icloud.com>
2015-12-22 22:33:22 +0100

    Added print() Field function
    Includes filling the hash map in constructor.

--- src/nl/Steffion/SeaBattle/Field.java
+++ src/nl/Steffion/SeaBattle/Field.java
@@ -4,9 +4,14 @@ import java.util.HashMap;
 
 public class Field {
 	private HashMap<String, Square> grid;
-	
+
 	public Field() {
 		grid = new HashMap<String, Square>();
+		for (char horizontal = 'A'; horizontal <= 'J'; horizontal++) {
+			for (int vertical = 10; vertical >= 0; vertical--) {
+				grid.put(horizontal + vertical + "", new Square());
+			}
+		}
 	}
 	
 	public void print() {
@@ -18,6 +23,27 @@ public class Field {
 	}
 	
 	public void allShipsSunk() {
+
+	public void print() {
+		// TODO print field
+		for (int vertical = 10; vertical >= 1; vertical--) {
+			if (vertical == 10) {
+				System.out.print(vertical);
+			} else {
+				System.out.print(vertical + " ");
+			}
+			
+			for (char horizontal = 'A'; horizontal <= 'J'; horizontal++) {
+				if (grid.get(horizontal + vertical + "").getShip() != null) {
+					System.out.print(" " + grid.get(horizontal + vertical + "").getShip().getTypeShip());
+				} else {
+					System.out.print(" .");
+				}
+			}
+			
+			System.out.println();
+		}
 		
+		System.out.println("   A B C D E F G H I J");
 	}
 }
