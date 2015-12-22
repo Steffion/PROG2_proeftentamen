fba08a96bc720b744986757730f82936bb98ada4
Steffion <steffion@icloud.com>
2015-12-22 22:28:15 +0100

    Added Player getters and setters

--- src/nl/Steffion/SeaBattle/Player.java
+++ src/nl/Steffion/SeaBattle/Player.java
@@ -1,6 +1,26 @@
 package nl.Steffion.SeaBattle;
 
 public class Player {
-	private String name;
-	private Field field;
+	private Field	field;
+	private String	name;
+	
+	public Player() {
+		field = new Field();
+	}
+					
+	public Field getField() {
+		return field;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setField(Field field) {
+		this.field = field;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
