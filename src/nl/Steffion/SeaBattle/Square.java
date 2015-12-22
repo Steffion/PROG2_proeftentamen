017967bb6323184bc6fd24f0f7287db8a2311312
Steffion <steffion@icloud.com>
2015-12-22 22:35:40 +0100

    Added Square getters and setters

--- src/nl/Steffion/SeaBattle/Square.java
+++ src/nl/Steffion/SeaBattle/Square.java
@@ -1,11 +1,27 @@
 package nl.Steffion.SeaBattle;
 
 public class Square {
-	private Ship ship;
-	private boolean shot;
-	
+	private Ship	ship;
+	private boolean	shot;
+
 	public Square() {
 		ship = null;
 		shot = false;
+	}
+
+	public Ship getShip() {
+		return ship;
+	}
+
+	public boolean isShot() {
+		return shot;
+	}
+
+	public void setShip(Ship ship) {
+		this.ship = ship;
+	}
+	
+	public void setShot(boolean shot) {
+		this.shot = shot;
 	}
 }
